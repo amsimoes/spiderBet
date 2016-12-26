3840048e958e08eea8c97ccde2b1a10987ad2510
A Simoes <asimoes@student.dei.uc.pt>
2016-12-26 18:04:50 +0000

    begin spider only to retrieve scores with custom pipeline

--- spiderbet/pipelines.py
+++ spiderbet/pipelines.py
@@ -4,8 +4,13 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import pygsheets
 
 
 class SpiderbetPipeline(object):
-    def process_item(self, item, spider):
-        return item
+	def process_item(self, item, spider):
+		if spider.name == 'bets':
+			with open('/home/bequis/spiderBet/bets.txt', 'a') as f:
+				f.write(item['unique_id']+" | "+item['date']+" | "+item['match']+" | "+item['tip']+" | "+item['odd']+'\n')
+		else:
+			return item
--- spiderbet/spiders/bets_spider.py
+++ spiderbet/spiders/bets_spider.py
@@ -1,18 +1,20 @@
 import scrapy
+import time
 
 class betSpider(scrapy.Spider):
 	name = "bets"
 
 	def start_requests(self):
-		self.games = dict()
 		urls = ["https://www.academiadasapostas.com"]
+		with open('/home/bequis/spiderBet/bets.txt', 'a') as f:
+			f.write("\n\n")
+			f.write(time.strftime("%d/%m/%Y")+"\n")
 		for url in urls:
 			yield scrapy.Request(url=url, callback=self.parse)
 
 
 	def parse(self, response):
 		fixtures = response.xpath('//div[contains(@id, "fh_main_tab")]')
-
 		for game in fixtures.xpath('.//tr[contains(@type, "match")]'):
 			if game.xpath('./td[contains(@class,"status")]/a').extract():
 				g = Game()
@@ -21,6 +23,7 @@ class betSpider(scrapy.Spider):
 				home_team = str(game.xpath('./td[contains(@class,"team-a")]/p/text()').extract())
 				away_team = str(game.xpath('./td[contains(@class,"team-b")]/p/text()').extract())
 				g['match'] = home_team[2:-2]+" X "+away_team[2:-2]
+				g['unique_id'] = link_game.split("/")[-3]
 				yield scrapy.Request(url=link_game, callback=self.parse_tip, meta=g)
 
 
@@ -38,5 +41,6 @@ class Game(scrapy.Item):
 	link = scrapy.Field()
 	match = scrapy.Field()
 	date = scrapy.Field()
+	unique_id = scrapy.Field()
 	tip = scrapy.Field()
 	odd = scrapy.Field()
